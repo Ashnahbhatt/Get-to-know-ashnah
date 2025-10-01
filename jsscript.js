7b21b4d6757ab4eb22c4c0e73d97ae135c74cc63
Ashnahbhatt <87618371+Ashnahbhatt@users.noreply.github.com>
2025-10-01 19:08:18 +0100

    Add files via upload

---
+++ jsscript.js
@@ -0,0 +1,5 @@
+// small helpers
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// if you want to add small interactivity later (project filters, nav toggle),
+// place JS here.
